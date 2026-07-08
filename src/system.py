76821bc80e37257fed9b56c755be7705efe22744
Rouge Forge <rougeforge01@gmail.com>
2026-07-08 07:38:42 +0100

    Update system.py

--- src/system.py
+++ src/system.py
@@ -1,1 +1,7 @@
+def get_os_name():
+    with open("/etc/os-release", "r") as file:
+        for line in file:
+            if line.startswith("PRETTY_NAME="):
+                return line.split("=", 1)[1].strip().strip('"')
 
+    return "Unknown Linux"
